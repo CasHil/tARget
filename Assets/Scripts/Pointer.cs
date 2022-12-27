e1ec383378406ab58aade0a5191d08d091558333
Casper Hildebrand <casperhildebrand@gmail.com>
2022-12-27 13:07:01 -0500

    Draw single line indicating pointer direction

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -7,11 +7,15 @@ public class Pointer : MonoBehaviour
     [SerializeField]
     private FingerInfoGizmo fingerInfoGizmo;
 
-    private Vector3 denormalizedPosition;
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public int lengthOfLineRenderer = 20;
+    public LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     private void Start()
     {
+        lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
         if (fingerInfoGizmo == null)
         {
             try
@@ -30,20 +34,37 @@ public class Pointer : MonoBehaviour
     {
         /*        fingerInfoGizmo.ShowFingerInformation();*/
         /*denormalizedPosition = DenormalizeJointValues(ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.skeleton.joints[8], Screen.width, Screen.height);*/
-        Vector3 ringPlacement = JointToHandTracking(ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.skeleton.joints[8]);
+        Vector3[] joints = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.skeleton.joints;
+        Vector3 jointIndexFinger1 = TranslateHandTrackingToWorldView(joints[6]);
+        Debug.Log("Joint index 1: " + jointIndexFinger1);
+        Vector3 jointIndexFinger2 = TranslateHandTrackingToWorldView(joints[7]);
+        Debug.Log("Joint index 2: " + jointIndexFinger2);
+
+        Vector3 aimDirection = (jointIndexFinger2 - jointIndexFinger1) * 10;
+        Debug.Log("aimDirection: " + aimDirection);
+
+        lineRenderer.startColor = Color.black;
+        lineRenderer.endColor = Color.black;
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.positionCount = 2;
+        lineRenderer.useWorldSpace = true;
+        /*LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.startWidth = 1.0f;
+        lineRenderer.endWidth = 1.0f;*/
+        lineRenderer.SetPosition(0, jointIndexFinger1);
+        lineRenderer.SetPosition(1, aimDirection);
+
+        /*Vector3 ringPlacement = JointToHandTracking(ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.skeleton.joints[8]);
 
         Debug.Log("Finger info at: " + ringPlacement);
-        GameObject.Find("Cube").transform.position = ringPlacement;
+        GameObject.Find("Cube").transform.position = ringPlacement;*/
     }
 
     // The hand tracking on the bottom left of the screen starts at (0, 0), i.e. if a joint is in the bottom left, its position is (0, 0).
     // However, the world origin is in the middle of the screen. Therefore, you need to translate the object's width by (-0.5, -0.5).
-    private Vector3 JointToHandTracking(Vector3 position)
+    private Vector3 TranslateHandTrackingToWorldView(Vector3 position)
     {
-        Vector3 calculatedJoint;
-        /* calculatedJoint = new Vector3(position.x * width, position.y * height, position.z);*/
-
-        calculatedJoint = new Vector3(position.x - 0.5f, position.y - 0.5f, position.z);
-        return calculatedJoint;
+        return new Vector3(position.x - 0.5f, position.y - 0.5f, position.z);
     }
 }
