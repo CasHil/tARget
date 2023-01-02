16d528aa673752e939650b84bd7cea3b70c930e4
Patrik Lundqvist <61392121+palundqv@users.noreply.github.com>
2023-01-02 15:31:22 +0100

    Added turrets

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
+    public GameObject turret;
     private GameObject cylinder;
-
+    private GameObject target;
     // Start is called before the first frame update
     private void Start()
     {
-        ManomotionManager.Instance.ShouldCalculateSkeleton3D(true);
-        cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        //ManomotionManager.Instance.ShouldCalculateSkeleton3D(true);
+        //cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        turret = Instantiate(turret);
     }
 
     // Update is called once per frame
@@ -22,25 +24,29 @@ public class Pointer : MonoBehaviour
         // Manomotion SDK Pro Technical Documentation for which joints 5 and 7 are.
         Vector3 startJoint = CalculateNewPositionFromJoint(joints[5]);
         Vector3 endJoint = CalculateNewPositionFromJoint(joints[7]);
-
+        
         Vector3 aimDirection = endJoint - startJoint;
         Vector3 position = startJoint + aimDirection * 2.0f;
         Vector3 scale = new Vector3(0.02f, aimDirection.magnitude * 2.0f, 0.02f);
-
+        
         /*
+        //turret.transform.rotation = target.transform.rotation;
+        turret.transform.rotation = Quaternion.LookRotation(aimDirection);
+        turret.transform.position = CalculateNewPositionFromJoint(joints[6]);
+        
         Debug.Log("Start joint: " + startJoint);
         Debug.Log("End joint: " + endJoint);
         Debug.Log("Aim direction: " + aimDirection);
         Debug.Log("Position: " + position);
         Debug.Log("Scale: " + scale);
         Debug.Log("======================= " + aimDirection);
-        */
         
         Destroy(cylinder.GetComponent<Rigidbody>()); //The missile gots destroyed by the cylinders rigidbody so I took it away lol
         cylinder.transform.position = position;
         cylinder.transform.localScale = scale;
         cylinder.transform.up = aimDirection;
-        
+        */
+
         // This is for testing, we can remove it when we don't want to fire by the touchscreen. 
         if(Input.touchCount == 0)
         {
@@ -48,7 +54,7 @@ public class Pointer : MonoBehaviour
         }
         else
         {   
-            GetComponent<instantiateProjectile>().Fire(position, aimDirection);
+            GetComponent<instantiateProjectile>().Fire(CalculateNewPositionFromJoint(joints[6]), aimDirection);
         }
     }
     private Vector3 CalculateNewPositionFromJoint(Vector3 joint)
